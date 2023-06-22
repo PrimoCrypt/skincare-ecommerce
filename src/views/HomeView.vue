1e2cf1104cc5a631e4e98ac237f286fa21827775
PrimoCrypt <104681010+PrimoCrypt@users.noreply.github.com>
2023-06-22 07:24:16 +0100

    Feat: Completed the Home first view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,65 +1,69 @@
 <template>
-  <div class="home">
-    <div class="homeHeader">
-      <NavBar />
-      <div class="homeHeaderMid">
-        <h3>{{ pageName }} : Skin for days</h3>
-        <p>Skin care us a science, {{ pageName }} makes it art. Reduce finelines and wrinkles with alpha hydroxy acid skin care products. Say HI to a new found glow routine.</p>
-        <button @click="navigateToProductView">Shop now</button>
-      </div>
+    <div class="home">
+        <div class="homeHeader">
+            <NavBar />
+            <div class="homeHeaderMid">
+                <h3>{{ pageName }} : Skin for days</h3>
+                <p>Skin care us a science, {{ pageName }} makes it art. Reduce finelines and wrinkles with alpha hydroxy
+                    acid skin care products. Say HI to a new found glow routine.</p>
+                <button @click="navigateToProductView">Shop now</button>
+            </div>
+        </div>
+        <div class="homeAboutUs">
+            <AboutUs/>
+        </div>
     </div>
-  </div>
-  <FooterImage />
+    <Footer />
 </template>
 
 <script>
-import FooterImage from '@/components/footer/FooterImage.vue'
+import AboutUs from '@/components/AboutUs.vue'
+import Footer from '@/components/Footer.vue'
 import pageData from '@/components/pageData.js'
 import NavBar from '@/components/NavBar.vue'
-import { useRouter,useRoute } from 'vue-router';
+import { useRouter, useRoute } from 'vue-router';
 
 export default {
-  name: 'Home',
-  components: { NavBar, FooterImage},
-  setup(){
-    const { pageName } = pageData()
-    const router = useRouter();
-    const route = useRoute();
-    const navigateToProductView = ()=>{
-        router.push('/products')
-    }
+    name: 'Home',
+    components: { NavBar, Footer , AboutUs },
+    setup() {
+        const { pageName } = pageData()
+        const router = useRouter();
+        const route = useRoute();
+        const navigateToProductView = () => {
+            router.push('/products')
+        }
 
-    
-    return{ pageName, navigateToProductView }
-  }
+
+        return { pageName, navigateToProductView }
+    }
 }
 </script>
 
 <style scoped>
-
-.homeHeader{
-  height: 120vh;
-  background: linear-gradient(rgba(0, 0, 0, 0.372), rgba(0, 0, 0, 0.194)), url('/src/assets/HomeHeaderImage.jpeg') no-repeat;
-  background-size: cover;
-  padding: 0 100px;
+.homeHeader {
+    height: 120vh;
+    background: linear-gradient(rgba(0, 0, 0, 0.372), rgba(0, 0, 0, 0.194)), url('/src/assets/HomeHeaderImage.jpeg') no-repeat;
+    background-size: cover;
+    padding: 0 100px;
 
 }
 
-.homeHeaderMid{
-  width: 60%;
-  text-align: left;
+.homeHeaderMid {
+    width: 60%;
+    text-align: left;
 }
 
-.homeHeaderMid h3{
-  font-size: 4rem;
-  margin: 180px 0 40px 0;
+.homeHeaderMid h3 {
+    font-size: 4rem;
+    margin: 180px 0 40px 0;
 }
 
-.homeHeaderMid p{
-  font-size: 1.4rem;
+.homeHeaderMid p {
+    font-size: 1.4rem;
 }
 
-.homeHeaderMid button{
+.homeHeaderMid button {
     border: none;
     background: #2F4333;
     color: white;
@@ -67,5 +71,9 @@ export default {
     border-radius: 3px;
     font-weight: bold;
     padding: 20px 50px;
+}
+
+.homeAboutUs{
+    height: 100vh;
 }
 </style>
