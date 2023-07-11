0aad9e1c10a7d37ff899ea8f4f3d82754168382d
PrimoCrypt <104681010+PrimoCrypt@users.noreply.github.com>
2023-07-11 09:08:21 +0100

    added harmburger menu

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,7 +1,8 @@
 <template>
-    <div class="navbar">
+    <div class="totalNavBar">
+        <div v-if="!isSmallScreen" class="navbar content">
             <h1>{{ pageNameHeader }}.</h1>
-            <div class="nav-link-center">
+            <div class="nav-links">
                 <router-link to="/">Home</router-link>
                 <router-link to="/products">Products</router-link>
                 <span>Sale</span>
@@ -11,21 +12,64 @@
             <span>
                 <router-link to="/cart">Cart({{ totalCartItems }})</router-link>
             </span>
+        </div>
+        
+        <div v-else class="navbar content">
+            <h1>{{ pageNameHeader }}.</h1>
+            <span>
+                <router-link to="/cart">Cart({{ totalCartItems }})</router-link>
+            </span>
+            <div class="whole-harmburger-menu">
+                <button class="hamburger-menu" @click="toggleMenu" :class="{ 'active': isMenuOpen }">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </button>
+                <div class="nav-links" :class="{ show: isMenuOpen }">
+                    <router-link to="/">Home</router-link>
+                    <router-link to="/products">Products</router-link>
+                    <span>Sale</span>
+                    <router-link to="/blog">Blog</router-link>
+                    <router-link to="/about">About Us</router-link>
+                </div>
+            </div>
+        </div>
     </div>
+        
     
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref, onMounted, onUnmounted } from 'vue'
 import pageData from './pageData.js'
 
 export default{
     setup(){
         const { pageNameHeader } = pageData()   
         const totalCartItems = ref(0)
+        const isMenuOpen = ref(false)
+
+        const toggleMenu = () => {
+            isMenuOpen.value = !isMenuOpen.value
+        }
+
+        const isSmallScreen = ref(window.innerWidth < 700);
+
+        const handleResize = () => {
+            isSmallScreen.value = window.innerWidth < 700;
+        };
+
+        onMounted(() => {
+            window.addEventListener('resize', handleResize);
+        });
+
+        onUnmounted(() => {
+            window.removeEventListener('resize', handleResize);
+        });
 
 
-        return{ pageNameHeader , totalCartItems}
+
+        return{ pageNameHeader, totalCartItems, isMenuOpen, toggleMenu, isSmallScreen}
     }
 }
 </script>
@@ -36,20 +80,28 @@ export default{
     align-items: center;
     flex-direction: row;
     justify-content: space-between;
-    margin: 0 80px;
+    /* margin: 0 80px; */
 }
-/* 
+
+.navbar h1{
+    margin-left: 80px;
+}
+
 .nav-link-center {
     width: 100%;
 
-} */
+}
 
 /* nav h1 {
     float: left;
     width: 120px;
 } */
 
-.nav-link-center a{
+.nav-link{
+    margin: 0 20px;
+}
+
+.nav-link a{
     width: 100px;
 }
 
@@ -63,4 +115,67 @@ export default{
     font-weight: bold;
     text-decoration: underline;
 }
+
+
+@media (max-width: 800px) {
+    .navbar a {
+        margin: 0 10px;
+    }
+}
+
+
+@media (max-width: 750px) {
+    .navbar a {
+        margin: 0 5px;
+    }
+
+}
+
+
+@media (max-width: 700px) {
+    .navbar h1{
+        margin-left: 20px;
+    }
+    /* .whole-harmburger-menu{
+        align-self:baseline;
+    } */
+    .hamburger-menu {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        background: none;
+        border: none;
+        cursor: pointer;
+    }
+    .hamburger-menu span {
+        display: block;
+        width: 25px;
+        height: 3px;
+        margin-bottom: 5px;
+        background-color: #000;
+    }
+    .nav-links {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+    .nav-links a {
+        margin: 10px 0;
+        text-decoration: none;
+        color: #000;
+    }
+    /* Hide the navigation links by default */
+    .nav-links {
+        display: none;
+    }
+    /* Show the navigation links when the menu is open */
+    .nav-links.show {
+        display: flex;
+        flex-direction: column;
+        background-color: white;
+    }
+}
+
+
 </style>
